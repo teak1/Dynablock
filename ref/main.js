c5c1294f0f4b00b7ebc9f80baf9af5b5d5dcecb9
teak1 <trainian@gmail.com>
2017-05-12 13:44:36 -0700

    Update main.js

--- ref/main.js
+++ ref/main.js
@@ -1,5 +1,23 @@
-var s = confirm("request whitelist?");
+var thisURL = "teak1.github.io/Dynablock/blocker.js";
+function exec(){
+  var search = "";
+  var scr = document.getElementsByTagName("script");
+  alert(scr.length);
+  for(var i = 0;i<scr.length;i++){
+    try{
+      document.body.innerHTML+=scr[i].src;
+      if(scr[i].src.split("?").includes(thisURL)){
+        search=scr[i].src;
+      }
+    }catch(e){
+      document.body.innerHTML=e;
+    }
+  }
+  var interp = JSON.parse(search.split("").shift().join(""));
+  var s = confirm("request whitelist?");
 if(s){
    var url = window.location.origin;
    window.location="https://teak1.github.io/Dynablock/?"+JSON.stringify({"url":url});
 }
+}
+setTimeout(exec,1000);
